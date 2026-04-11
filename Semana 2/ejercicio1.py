7b616d4458ac90f6980ceb3734e73356139d8e52
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2026-04-10 19:50:46 -0500

    Create ejercicio1.py

---
+++ Semana 2/ejercicio1.py
@@ -0,0 +1,15 @@
+def mi_primera_funcion(n):
+    if n % 2: #0  (False)
+        print("El número es impar") 
+    else: 
+        print("El número es par") 
+
+
+
+# main
+n = 8
+mi_primera_funcion(n)
+n = 9
+mi_primera_funcion(n)
+n = 10
+mi_primera_funcion(n)
